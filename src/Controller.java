520cbfcf9f12435523f642a882d9a415058b445b
SergSemak <semakin.s.v@gmail.com>
2023-11-13 00:01:30 +0300

    added menu

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,9 @@
 import Model.Animal;
 import Model.AnimalCreator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -21,7 +24,8 @@ public class Controller {
     }
 
     public void createNewAnimal(){
-        try(Scanner scanner = new Scanner(System.in)){
+        try {
+            Scanner scanner = new Scanner(System.in);
             System.out.println("Creation of new animal...");
             System.out.println("Enter type of animal:");
             AnimalCreator creator = new AnimalCreator(scanner.nextLine());
@@ -37,6 +41,49 @@ public class Controller {
         } catch (Exception e){
             System.out.println("Invalid data");
             System.out.println(e.getMessage());
+        }
+    }
+
+    public void showMenu() throws IOException {
+        String menu = "Enter number of task:\n" +
+                "1 - Add new animal to registry\n" +
+                "2 - Show commands of certain animal\n" +
+                "3 - Train certain animal in new commands\n" +
+                "4 - Show list of animals by date of birthday\n" +
+                "5 - Show the total number of animals\n" +
+                "0 - exit";
+        System.out.println(menu);
+        try(BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in))) {
+            int num = Integer.parseInt(reader.readLine());
+            do {
+                switch (num) {
+                    case 1:
+                        createNewAnimal();
+                        break;
+                    case 2:
+                        System.out.println("Enter name of animal:");
+                        registry.showCommandsByName(reader.readLine());
+                        break;
+                    case 3:
+                        ;
+                        break;
+                    case 4:
+                        registry.showAllAnimalsByBirthday();
+                        break;
+                    case 5:
+                        System.out.println("Count of all animals: " + registry.countOfAnimals());
+                        break;
+                    case 0:
+                        System.out.println("Goodbye!");
+                        System.exit(0);
+                    default:
+                        System.out.println("No action");
+                }
+                System.out.println(menu);
+                System.out.println();
+                num = Integer.parseInt(reader.readLine());
+            } while (num != 0);
         }
     }
 
--- src/Program.java
+++ src/Program.java
@@ -3,16 +3,15 @@ import Model.AnimalCreator;
 import Model.Cat;
 import Model.Dog;
 
+import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-//TODO: сделать меню
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Animal cat = new Cat("Bublic",
                 LocalDate.parse("23.12.2020", DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ROOT)),
                 List.of("Fly"));
@@ -24,38 +23,15 @@ public class Program {
                 .setBirthday(LocalDate.now())
                 .setCommands(List.of("Roll", "Sit"))
                 .create();
-//        System.out.println(cat);
-
 
         List<Animal> animals = new ArrayList<>();
         animals.add(cat);
         animals.add(dog);
         animals.add(catC);
 
-        for(Animal a: animals){
-            System.out.println(a);
-        }
-
         Registry<Animal> registry = Registry.getRegistry();
-//        Registry<Animal> registry = new Registry<>();
-//        registry.addAnimalToRegistry(catC);
         registry.addListOfAnimals(animals);
-        registry.showCommandsByName(catC.getName());
-        registry.showCommandsById(1);
-
-//        catC.learnCommand("Jump");
-//        registry.showCommandsByName(catC.getName());
-
-//        Controller controller = new Controller();
-//        controller.setRegistry(registry);
-//        controller.createNewAnimal();
-//
-//        registry.showCommandsByName("Bob");
-
-        registry.showAllAnimals();
-        System.out.println("___________________");
-        registry.showAllAnimalsByBirthday();
-
-        System.out.println(registry.countOfAnimals());
+        Controller controller = new Controller(registry);
+        controller.showMenu();
     }
 }
